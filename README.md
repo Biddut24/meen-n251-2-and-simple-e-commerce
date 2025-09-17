0c3dbee0df026a0fcb8a6b8e4e87082f8db9f3cc
Mbdid <mbdid@example.com>
2025-09-17 11:04:20 +0600

    porject setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# simple e-commerce
+
+I am working with the project whichis is a wonder..... Currently, two official plugins are available:
+
+- [Live website link](www.live link comming) 
+
+- [Backend Api](https://restcountries.com/v3.1/all)
+## Api endpoints
+# /all-news
+# /users
+# /products
+# /product/id
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./components/Home/Home";
+import Footer from "./components/Shard/Footer/Footer";
+import Header from "./components/Shard/Header/Header";
+import Heder from "./components/Shard/Header/Header";
+
+function App() {
+  return (
+    <div>
+    <Header></Header>
+      <Home></Home>
+  <Footer></Footer>
+    </div>
+  );
+}
+export default App;
